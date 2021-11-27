af8c6b0afad794af8e24a15369240ee8e629be27
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-11-27 19:25:59 +0100

    nouvelle methode ds controller pour une reponse sans contenu

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -11,4 +11,14 @@ class Controller extends BaseController{
         // say to lumen "send a JSON response ; with specified status code
         return response()->json($data, $httpStatusCode);
     }
+    /**
+     * Send an empty response (no content, only status code)
+     *
+     * @param [type] $httpStatusCode
+     * @return void
+     */
+    public function sendEmptyResponse($httpStatusCode)
+    {
+        return response('', $httpStatusCode);
+    }
 }
